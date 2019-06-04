0dc213a56518d3b1b77387811b0ad2617cd0425f
powerbarrusa <barr@covrigaru.com>
2019-06-04 12:19:36 -0600

    storing

---
+++ main.ts
@@ -0,0 +1,14 @@
+var message = "Hello World"
+
+log(message)
+
+var number = 1
+let count = 2
+function doSomething() {
+  for (var i = 0; i < 5; i++) {
+    console.log(i)
+  }
+  console.log("Finally: " + i)
+}
+
+doSomething()
